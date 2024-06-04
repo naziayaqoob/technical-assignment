bee6fd3800a0dca4c4c8899af6269395b1ed57c2
naziayaqoob22 <nazia.yaqoob@ewave.com>
2024-06-04 16:18:35 +0500

    updated validation logic

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Button from './Button/Button';
 import HeroImg from '../assets/images/hero-banner-img-2.png';
 
@@ -47,25 +47,19 @@ const Registration = () => {
         setFormData({ name: '', company: '', email: '' });
     };
 
-    useEffect(() => {
-        if (submitted) {
-            const timer = setTimeout(() => setSubmitted(false), 5000);
-            return () => clearTimeout(timer);
-        }
-    }, [submitted]);
-
     const handleChange = (e) => {
         const { name, value } = e.target;
         setFormData((prevData) => ({
             ...prevData,
             [name]: value
         }));
+        setErrors({ ...errors, [name]: '' });
     };
 
     return (
         <section className="container max-w-custom mx-auto grid grid-cols-12 gap-8 mb-8 md:mb-24">
             <div className="lg:col-span-5 col-span-12">
-                <img src={HeroImg} alt="Image" className="w-full h-auto" />
+                <img src={HeroImg} alt="Hero Banner" className="w-full h-auto" />
             </div>
             <div className="lg:col-span-6 col-span-12 md:col-start-2 bg-white shadow-lg rounded-lg p-6 md:p-12">
                 <h2 className="text-2xl font-semibold mb-6 text-black">Registration</h2>
@@ -81,41 +75,41 @@ const Registration = () => {
                 <form onSubmit={handleSubmit}>
                     <div className="mb-6 mt-10">
                         <label htmlFor="name" className="block mb-2 text-black">Name <span className="text-red-500">*</span></label>
-                        <input 
-                            type="text" 
+                        <input
+                            type="text"
                             id="name"
                             placeholder="Enter your name"
                             name="name"
-                            value={formData.name} 
-                            onChange={handleChange} 
-                            required 
-                            className={`appearance-none rounded w-full leading-tight focus:outline-none focus:shadow-outline ${errors.name ? 'border-red-500' : ''}`} 
+                            value={formData.name}
+                            onChange={handleChange}
+                            required
+                            className={`appearance-none rounded w-full leading-tight focus:outline-none focus:shadow-outline ${errors.name ? 'border-red-500' : ''}`}
                         />
                         {errors.name && <p className="text-red-500 text-xs mt-1">{errors.name}</p>}
                     </div>
                     <div className="mb-6">
                         <label htmlFor="company" className="block mb-2 text-black">Company</label>
-                        <input 
-                            type="text" 
+                        <input
+                            type="text"
                             id="company"
                             placeholder="Enter your company name"
-                            name="company" 
-                            value={formData.company} 
-                            onChange={handleChange} 
-                            className="appearance-none rounded w-full leading-tight focus:outline-none focus:shadow-outline" 
+                            name="company"
+                            value={formData.company}
+                            onChange={handleChange}
+                            className="appearance-none rounded w-full leading-tight focus:outline-none focus:shadow-outline"
                         />
                     </div>
                     <div className="mb-6">
                         <label htmlFor="email" className="block mb-2 text-black">Email address <span className="text-red-500">*</span></label>
-                        <input 
-                            type="email" 
+                        <input
+                            type="email"
                             id="email"
                             placeholder="Enter your email address"
-                            name="email" 
-                            value={formData.email} 
-                            onChange={handleChange} 
-                            required 
-                            className={`appearance-none rounded w-full leading-tight focus:outline-none focus:shadow-outline ${errors.email ? 'border-red-500' : ''}`} 
+                            name="email"
+                            value={formData.email}
+                            onChange={handleChange}
+                            required
+                            className={`appearance-none rounded w-full leading-tight focus:outline-none focus:shadow-outline ${errors.email ? 'border-red-500' : ''}`}
                         />
                         {errors.email && <p className="text-red-500 text-xs mt-1">{errors.email}</p>}
                     </div>
